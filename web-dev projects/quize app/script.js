844e7ed220f47df66f65d65abe36148cd6f098a9
srishti132 <rastogisrishti514@gmail.com>
2026-07-19 16:45:20 +0530

    updated 4 questions

--- web-dev projects/quize app/script.js
+++ web-dev projects/quize app/script.js
@@ -10,12 +10,12 @@ const questions = [
         
     },
     {
-        question : "Which is largest animal in the world?",
+        question : "Which is smallest country in the world?",
         answers: [
-            {text: "shark", correct: false},
-            {text: "Blue whale", correct: true},
-            {text: "Elephant", correct: false},
-            {text: "Giraffe", correct: false},
+            {text: "Vetican city", correct: true},
+            {text: "Bhutan", correct: true},
+            {text: "Nepal", correct: false},
+            {text: "Shri lanka", correct: false},
         ]
         
     },
@@ -30,22 +30,22 @@ const questions = [
         
     },
      {
-        question : "Which is largest animal in the world?",
+        question : "Which is largest desert in the world?",
         answers: [
-            {text: "shark", correct: false},
-            {text: "Blue whale", correct: true},
-            {text: "Elephant", correct: false},
-            {text: "Giraffe", correct: false},
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: true},
+            {text: "Antartica", correct: false},
         ]
         
     },
      {
-        question : "Which is largest animal in the world?",
+        question : "Which is smallest continent in the world?",
         answers: [
-            {text: "shark", correct: false},
-            {text: "Blue whale", correct: true},
-            {text: "Elephant", correct: false},
-            {text: "Giraffe", correct: false},
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Artic", correct: false},
+            {text: "Africa", correct: false},
         ]
         
     }
