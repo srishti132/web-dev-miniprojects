e7f0f0123aef7fddeaf84dd48fdd749e10d6af7b
srishti132 <rastogisrishti514@gmail.com>
2026-07-19 15:56:53 +0530

    Initial quiz app structure

---
+++ web-dev projects/quize app/script.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        question : "Which is largest animal in the world?",
+        answers: [
+            {text: "shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+        
+    },
+    {
+        question : "Which is largest animal in the world?",
+        answers: [
+            {text: "shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+        
+    },
+     {
+        question : "Which is largest animal in the world?",
+        answers: [
+            {text: "shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+        
+    },
+     {
+        question : "Which is largest animal in the world?",
+        answers: [
+            {text: "shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+        
+    },
+     {
+        question : "Which is largest animal in the world?",
+        answers: [
+            {text: "shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+        
+    }
+]
