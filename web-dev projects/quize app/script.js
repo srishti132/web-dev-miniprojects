ed6b73b4555b923c81087854837a84dd644d0146
srishti132 <rastogisrishti514@gmail.com>
2026-07-20 20:32:49 +0530

    added first quiz question

--- web-dev projects/quize app/script.js
+++ web-dev projects/quize app/script.js
@@ -13,7 +13,7 @@ const questions = [
         question : "Which is smallest country in the world?",
         answers: [
             {text: "Vetican city", correct: true},
-            {text: "Bhutan", correct: true},
+            {text: "Bhutan", correct: false},
             {text: "Nepal", correct: false},
             {text: "Shri lanka", correct: false},
         ]
@@ -50,3 +50,53 @@ const questions = [
         
     }
 ]
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+// function showQuestion(){
+//     let currentQuestion = questions[currentQuestionIndex];
+//     let questionNo = currentQuestionIndex + 1;
+//     questionElement.innerHTML = questionNo + ". "+ currentQuestion.question;
+
+//     currentQuestion.answers.array.forEach(answer => {
+//         const button = document.createElement("button");
+//         button.innerHTML = answer.text;
+//         button.classList.add("btn");
+//         answerButton.appendChild(button);
+        
+//     });
+// }
+//  startQuiz();
+
+function showQuestion() {
+    resetState()
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButton.firstChild){
+        answerButton.removeChild(answerButton.firstChild)
+    }
+}
+startQuiz();
